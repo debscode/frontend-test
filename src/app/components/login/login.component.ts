5cd356fed6cec176d770cdf4c0cc2745186513d1
Daniel <danielburbano19@gmail.com>
2020-12-12 21:27:21 -0500

    feat: request login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { Login } from 'src/app/types';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +14,7 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
@@ -23,25 +26,35 @@ export class LoginComponent implements OnInit {
    */
   createForm(): void {
     this.form = this.formBuilder.group({
-      email: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]]
     });
   }
 
-  get emailInvalid(){
+  get emailInvalid() {
     return this.form.get('email').invalid && this.form.get('email').touched;
   }
-  get passwordInvalid(){
+  get passwordInvalid() {
     return this.form.get('password').invalid && this.form.get('password').touched;
   }
 
-  submit(){
+  async submit() {
     if (this.form.invalid) {
       return Object.values(this.form.controls).forEach(control => {
-          control.markAsTouched();
+        control.markAsTouched();
       });
+    }    
+    const request: Login = {
+      email: this.form.controls.email.value,
+      password: this.form.controls.password.value
+    };
+    try {
+      const res: any = await this.userService.login(request);
+      localStorage.setItem('token', res.token);
+      localStorage.setItem('email', res.user.email);
+    } catch (error) {
+      console.log(error);
     }
-    console.log('valor', this.form.value);    
   }
 
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
+import { Login } from '../types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private _url = environment.url;
+  private _userToken: string;
+  private _userEmail: string;
+  private _paths: any = {
+    login: '/login',
+  }
+
+  constructor(private http: HttpClient) { }
+
+  readToken() {
+    this._userToken = localStorage.getItem('token') ? localStorage.getItem('token') : '';
+    return this._userToken;
+  }
+
+  readEmail() {
+    this._userEmail = localStorage.getItem('email') ? localStorage.getItem('email') : '';
+    return this._userEmail;
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+    this._userToken = token;
+  }
+
+  saveEmail(email: string) {
+    localStorage.setItem('email', email);
+    this._userEmail = email;
+  }
+
+  isAuth(): boolean {
+    this.readToken();
+    return this._userToken.length > 2;
+  }
+
+  login(req: Login) {
+    const body = new HttpParams()
+      .set('email', req.email)
+      .set('password', req.password);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded'
+      })
+    };
+
+    return new Promise((res, rej) => {
+      this.http.post(`${this._url}${this._paths.login}`, body.toString(), httpOptions)
+        .subscribe((data: any) => {
+          data.status.error ? rej(data.message) : res(data.data);
+        }, (error: any) => rej(error));
+    });
+  }
+
+}
