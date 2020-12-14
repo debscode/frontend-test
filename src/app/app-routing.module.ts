0d649f0db39c5fb2784823e7a4bc5575cf18a644
Daniel <danielburbano19@gmail.com>
2020-12-13 19:28:38 -0500

    feat: add guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccessGuard } from './guards/access.guard';
+import { UserGuard } from './guards/user.guard';
 
 const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule),
+    canActivate: [AccessGuard]
   },
   {
     path: 'main',
     loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule),
+    canActivate: [UserGuard]
   },
   {
     path: '**',
